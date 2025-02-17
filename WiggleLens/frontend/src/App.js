43aa2e6410bec18728ca1bce97f7f4f6a35d8900
catleonidas <37508051+catleonidas@users.noreply.github.com>
2025-02-17 13:54:05 +0000

    http

--- WiggleLens/frontend/src/App.js
+++ WiggleLens/frontend/src/App.js
@@ -270,8 +270,8 @@ function App() {
       // We do /api/process_image_no_rgba if useSecondFrameAsBackground is true,
       // otherwise /api/process_image.
       const endpoint = useSecondFrameAsBackground
-        ? 'https://139.162.152.41:8000/api/process_image_no_rgba'
-        : 'https://139.162.152.41:8000/api/process_image';
+        ? 'http://139.162.152.41:8000/api/process_image_no_rgba'
+        : 'http://139.162.152.41:8000/api/process_image';
 
       const response = await fetch(endpoint, {
         method: 'POST',
